3a9fb978d917df9546c1e82af7db2aab42f98913
Rdm-jony <jonydascse@gmail.com>
2025-06-20 00:44:03 +0600

    complete borrow and book api requirement

---
+++ src/model/borrow_model.ts
@@ -0,0 +1,19 @@
+import { Model, model, Schema } from "mongoose";
+import { BorrowMethod, IBorrow } from "../interface/borrow_interface";
+import { Book } from "./book_model";
+
+const borrowShema = new Schema<IBorrow, Model<IBorrow>, BorrowMethod>({
+    book: { type: Schema.Types.ObjectId, ref: "Book", required: [true, "book_id is required"] },
+    quantity: { type: Number, min: [1, "select at least 1 quantity"], required: [true, "quantity is required"] },
+    dueDate: { type: Date, required: [true, "dueDate is required"] }
+}, {
+    versionKey: false,
+    timestamps: true
+})
+
+borrowShema.method("updateAvailableBook", async function (bookId: Schema.Types.ObjectId) {
+   
+    await Book.findByIdAndUpdate(bookId, { $set: { available: false } })
+})
+
+export const Borrow = model("Borrow", borrowShema)
